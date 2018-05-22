485120d1286e371e692e6f751940f1ba71d2b33c
williamorim <thewilliam89@gmail.com>
2018-05-22 10:09:46 -0300

    Apresentação Rday

---
+++ exemplos/app_1.R
@@ -0,0 +1,12 @@
+## Template
+
+library(shiny)
+
+ui <- fluidPage('Hello World')
+
+
+server <- function(input, output) {
+
+}
+
+shinyApp(ui=ui, server = server)
---
+++ exemplos/app_3.R
@@ -0,0 +1,67 @@
+# Load the ggplot2 package which provides
+# the 'mpg' dataset.
+
+library(shiny)
+library(ggplot2)
+
+ui <- fluidPage(
+  titlePanel("Basic DataTable"),
+  
+  # Create a new Row in the UI for selectInputs
+  fluidRow(
+    column(
+      width = 4,
+      selectInput(
+        inputId = "man",
+        label = "Manufacturer:",
+        choices = c("All",unique(as.character(mpg$manufacturer)))
+      )
+    ),
+    column(
+      width = 4,
+      selectInput(
+        inputId = "trans",
+        label = "Transmission:",
+        c("All", unique(as.character(mpg$trans))))
+    ),
+    column(
+      width = 4,
+      selectInput(
+        inputId = "cyl",
+        label = "Cylinders:",
+        c("All", unique(as.character(mpg$cyl))))
+    )
+  ),
+  
+  # Create a new row for the table.
+  fluidRow(
+    DT::dataTableOutput("table")
+  )
+)
+
+server <- function(input, output) {
+  
+  # Filter data based on selections
+  output$table <- DT::renderDataTable(DT::datatable({
+    
+    data <- mpg
+    
+    if (input$man != "All") {
+      data <- data[data$manufacturer == input$man,]
+    }
+    
+    if (input$cyl != "All") {
+      data <- data[data$cyl == input$cyl,]
+    }
+    
+    if (input$trans != "All") {
+      data <- data[data$trans == input$trans,]
+    }
+    
+    data
+    
+  }))
+  
+}
+
+shinyApp(ui, server)
